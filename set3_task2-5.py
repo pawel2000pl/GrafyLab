c267986293eeebb8148d8b2178b8a84e8c3538b0
Paweł Bielecki <pawelek24@op.pl>
2022-05-23 11:20:45 +0200

    Dodano przykłady do zadania 2 i 5

--- set3_task2-5.py
+++ set3_task2-5.py
@@ -35,3 +35,69 @@ for v in vertexList:
 print("\nSpanning tree")
 print([str(edge) for edge in g.minimalSpanningTree("c")])
 
+print("\n\n\n\n\n")
+
+g = Graph(False)
+for i in range(1, 7):
+    g.addVertex()
+
+g.addEdge("v1", "v4", weight=4)
+g.addEdge("v1", "v3", weight=1)
+g.addEdge("v1", "v5", weight=4)
+g.addEdge("v1", "v6", weight=9)
+g.addEdge("v2", "v3", weight=9)
+g.addEdge("v2", "v6", weight=6)
+g.addEdge("v3", "v5", weight=7)
+g.addEdge("v4", "v5", weight=9)
+g.addEdge("v4", "v6", weight=4)
+g.addEdge("v5", "v6", weight=7)
+
+vertexList = list(g.vertexIndex.keys())
+for v in vertexList:
+    print("\n", v, ": ", g.DijkstraDistance(v))
+    for v2 in vertexList:
+        way = g.getWayTo(v2)
+        print([str(edge) for edge in way], " length = ", sum(edge.weight for edge in g.getWayTo(v2)))
+
+
+print("\nSpanning tree")
+print([str(edge) for edge in g.minimalSpanningTree()])
+
+print("\n\n\n\n\n")
+
+g = Graph(False)
+
+for i in range(1, 13):
+    g.addVertex()
+    
+g.addEdge("v1", "v2", weight=3)
+g.addEdge("v4", "v2", weight=2)
+g.addEdge("v4", "v7", weight=3)
+g.addEdge("v7", "v10", weight=5)
+g.addEdge("v1", "v5", weight=9)
+g.addEdge("v5", "v2", weight=4)
+g.addEdge("v5", "v7", weight=1)
+g.addEdge("v5", "v8", weight=2)
+g.addEdge("v8", "v10", weight=5)
+g.addEdge("v12", "v8", weight=9)
+g.addEdge("v1", "v3", weight=2)
+g.addEdge("v3", "v5", weight=6)
+g.addEdge("v3", "v6", weight=9)
+g.addEdge("v6", "v8", weight=1)
+g.addEdge("v6", "v9", weight=2)
+g.addEdge("v9", "v11", weight=2)
+g.addEdge("v11", "v12", weight=3)
+g.addEdge("v8", "v11", weight=6)
+print("\n\n\n")
+print(str(g))
+vertexList = list(g.vertexIndex.keys())
+for v in vertexList:
+    print("\n", v, ": ", g.DijkstraDistance(v))
+    for v2 in vertexList:
+        way = g.getWayTo(v2)
+        print([str(edge) for edge in way], " length = ", sum(edge.weight for edge in g.getWayTo(v2)))
+
+
+print("\nSpanning tree")
+print([str(edge) for edge in g.minimalSpanningTree()])
+
